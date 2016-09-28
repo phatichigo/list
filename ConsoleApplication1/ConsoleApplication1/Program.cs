f8f27638ce63d7098e9ce24a542ff5e71ed0dc59
Thach Phat <kutebuk1102@gmail.com>
2016-09-28 08:39:11 +0700

    test

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.String;
+
+namespace ConsoleApplication1
+{
+    class BaiDauTien
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MO ta huong doi tuong trong C#");
+            Console.WriteLine("--------**--------");
+            //tao doi tuong trong C#
+            Rectangle r =new Rectangle();
+
+            //goi phuong thuc
+            r.Acc();
+            r.Display();
+
+            //Tu khoa sizeof trong C#
+
+            Console.WriteLine("Kich co cua kieu du lieu int la:{0}",sizeof(int));
+            Console.WriteLine("Kich co cua kieu du lieu double la:{0}", sizeof(double));
+            Console.WriteLine("Kich co cua kieu du lieu float la:{0}", sizeof(float));
+            Console.WriteLine("Kich co cua kieu du lieu char la:{0}", sizeof(char));
+            Console.WriteLine("Kich co cua kieu du lieu bool la:{0}", sizeof(bool));
+            Console.WriteLine("Kich co cua kieu du lieu ulong la:{0}", sizeof(ulong));
+            Console.WriteLine("Kich co cua kieu du lieu sbyte la:{0}", sizeof(sbyte));
+
+
+
+
+
+
+
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Rectangle
+    {
+        //khaibao bien;
+        double len, wid;
+
+
+        //Phuongthuc
+        public void Acc()
+        {
+            len = 10.5;
+            wid = 15.5;
+        }
+        //Phuong thuc  get
+        public double GetArea()
+        {
+            return len * wid;
+        }
+        //Phuong thuc in ra man hinh
+        public void Display()
+        {
+            Console.WriteLine("ChieuDai:{0}", len);
+            Console.WriteLine("chieurong:{0}", wid);
+            Console.WriteLine("Dientich:{0}", GetArea());
+
+
+        }
+    }
+}
